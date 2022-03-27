a7afb7b9b0babab1c60d3590fa249e462a3e1047
S H Chan <shchan@Ss-MacBook-Pro.local>
2022-03-27 20:08:02 +0100

    update

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +17,17 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+//types of array
+
+var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z"]
+var uppercase = lowercase.toUpperCase();
+
+console.log(uppercase)
+
+
+//prompt when click generate password
+//length of the password 8 characters and no more than 128 characters
+//lowercase, uppercase, numeric, and/or special characters

c2710b57dcd9b8344891f0b04ede83fba66ba7ae
shingchan95 <chanhei888@gmail.com>
2022-03-28 21:13:03 +0100

    update

--- script.js
+++ script.js
@@ -27,7 +27,10 @@ var uppercase = ['A','B','C','D','E','F','G','H','I','J','J','L','M','N','O','P'
 var specialCharacters = ["!",'"',"#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@",'[',"\\",']','^',"_","`","{","|","}","~"]
 var numeric=['1','2','3','4','5','6','7','8','9','0']
 var prePassword=[]
-
+var newPassword=[]
+var newPasswordindex=[]
+var finalPassword=[]
+var i=0
 
 //prompt when click generate password
 
@@ -46,51 +49,58 @@ while (passwordLength <8 || passwordLength>128)
 
   var quesLowercase= window.confirm("Including lowercase?")
   if (quesLowercase){
-    prePassword=prePassword.concat(lowercase);
+    prePassword= prePassword.concat(lowercase);
+    lowercaseIndex= (Math.floor(Math.random() * lowercase.length));
+    newPassword.push(lowercase[lowercaseIndex]);
+    i++
+
   }
 
   var quesUppercase= window.confirm("Including uppercase?")
   if (quesUppercase){
     prePassword=prePassword.concat(uppercase);
+    uppercaseIndex= (Math.floor(Math.random() * uppercase.length));
+    newPassword.push(uppercase[uppercaseIndex]);
+    i++
   }
 
   var quesSpecial= window.confirm("Including specialCharacters?")
   if (quesSpecial){
     prePassword=prePassword.concat(specialCharacters);
+    specialcharactersIndex= (Math.floor(Math.random() * specialCharacters.length));
+    newPassword.push(specialCharacters[specialcharactersIndex]);
+    i++
   }
 
   var quesNumberic= window.confirm("Including numberic?")
   if (quesNumberic){
     prePassword=prePassword.concat(numeric);
+    numericIndex= (Math.floor(Math.random() * numeric.length));
+    newPassword.push(numeric[numericIndex]);
+    i++
   }
 
+  
 
-function findCommonElement(array1, array2) {
-  for(let i = 0; i < array1.length; i++) {
-      for(let j = 0; j < array2.length; j++) {
-          if(array1[i] === array2[j]) {
-              return true;
-          }
-      }
-  }
-  return false;
-}
-
-
-
-  var newPassword=[]
-
-  var i=0
-  while (i<passwordLength)
-  {
-    var newPasswordindex= (Math.floor(Math.random() * prePassword.length));
+  while (i<passwordLength){
+    newPasswordindex= (Math.floor(Math.random() * prePassword.length));
 
     newPassword.push(prePassword[newPasswordindex]);
 
     i++;
   }
-   
-console.log(newPassword);
+ 
+  var p=0
+  while (p < newPassword.length){
+    var finalPasswordindex=(Math.floor(Math.random() * newPassword.length));
+    finalPassword.push(newPassword[finalPasswordindex]);
+    p++;
+  }
+ 
+ 
+ 
+
+  console.log(finalPassword)
 
 //length of the password 8 characters and no more than 128 characters
 //lowercase, uppercase, numeric, and/or special characters

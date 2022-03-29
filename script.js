e8018be7e23520361a8f5978d2d8403bc6bfc0ad
shingchan95 <chanhei888@gmail.com>
2022-03-29 21:56:29 +0100

    update

--- script.js
+++ script.js
@@ -1,48 +1,49 @@
 
-
-
-//types of array
-
-var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-var uppercase = ['A','B','C','D','E','F','G','H','I','J','J','L','M','N','O','P','Q','R','S','T','U','V','w','X','Y','Z']
-var specialCharacters = ["!",'"',"#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@",'[',"\\",']','^',"_","`","{","|","}","~"]
-var numeric=['1','2','3','4','5','6','7','8','9','0']
-var prePassword=[]
-var newPassword=[]
-var newPasswordindex=[]
-var finalPassword=[]
-var i=0
-
-//prompt when click generate password
-
-
-
-var firstEntry = window.prompt("Length of the password (please input 8-128)")
-var passwordLength = parseInt(firstEntry)
-
-
-while (passwordLength <8 || passwordLength>128)
+function generatePassword()
 {
-  passwordLength= window.prompt("Error! please try again!"+"/n"+"Length of the password (please input 8-128)")
   
-}
+  //types of array
+  
+  var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+  var uppercase = ['A','B','C','D','E','F','G','H','I','J','J','L','M','N','O','P','Q','R','S','T','U','V','w','X','Y','Z']
+  var specialCharacters = ["!",'"',"#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@",'[',"\\",']','^',"_","`","{","|","}","~"]
+  var numeric=['1','2','3','4','5','6','7','8','9','0']
+  var prePassword=[]
+  var newPassword=[]
+  var newPasswordindex=[]
+  var finalPassword=[]
+  var i=0
+  
+  //prompt when click generate password
+  
+  
+  
+  var firstEntry = window.prompt("Length of the password (please input 8-128)")
+  var passwordLength = parseInt(firstEntry)
+  
+  
+  while (true) 
+    if (passwordLength <8 || passwordLength>128 || !passwordLength){
+       passwordLength= window.prompt("Error! please try again!"+"/n"+"Length of the password (please input 8-128)")}
 
+  else{
 
-var quesLowercase= window.confirm("Including lowercase?")
-if (quesLowercase){
-    prePassword= prePassword.concat(lowercase);
-    lowercaseIndex= (Math.floor(Math.random() * lowercase.length));
-    newPassword.push(lowercase[lowercaseIndex]);
-    i++
+    var quesLowercase= window.confirm("Including lowercase?")
+    if (quesLowercase){
+      prePassword= prePassword.concat(lowercase);
+      lowercaseIndex= (Math.floor(Math.random() * lowercase.length));
+      newPassword.push(lowercase[lowercaseIndex]);
+      i++
+      console.log(newPassword)
+    }
     
-  }
-  
-  var quesUppercase= window.confirm("Including uppercase?")
-  if (quesUppercase){
-    prePassword=prePassword.concat(uppercase);
-    uppercaseIndex= (Math.floor(Math.random() * uppercase.length));
+    var quesUppercase= window.confirm("Including uppercase?")
+    if (quesUppercase){
+      prePassword=prePassword.concat(uppercase);
+      uppercaseIndex= (Math.floor(Math.random() * uppercase.length));
     newPassword.push(uppercase[uppercaseIndex]);
     i++
+    console.log(newPassword)
   }
   
   var quesSpecial= window.confirm("Including specialCharacters?")
@@ -51,6 +52,7 @@ if (quesLowercase){
     specialcharactersIndex= (Math.floor(Math.random() * specialCharacters.length));
     newPassword.push(specialCharacters[specialcharactersIndex]);
     i++
+    console.log(newPassword)
   }
   
   var quesNumberic= window.confirm("Including numberic?")
@@ -59,6 +61,7 @@ if (quesLowercase){
     numericIndex= (Math.floor(Math.random() * numeric.length));
     newPassword.push(numeric[numericIndex]);
     i++
+    console.log(newPassword)
   }
   
   
@@ -71,18 +74,13 @@ if (quesLowercase){
     i++;
   }
   
-  var p=0
-  while (p < newPassword.length){
-    var finalPasswordindex=(Math.floor(Math.random() * newPassword.length));
-    finalPassword.push(newPassword[finalPasswordindex]);
-    p++;
-  }
-  
-  
-  
-  
-  console.log (finalPassword.join(""))
 
+  
+
+  return newPassword.join("")
+  
+}
+}
 
   
   // Assignment Code
